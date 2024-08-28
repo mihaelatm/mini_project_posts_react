8a3e795fd63a921d84d6339963cf2d29395c679f
Mihaela Talmazan <talmazanmihaela@gmail.com>
2024-08-28 12:16:35 +0200

    Added Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
+import React from "react";
 import styles from "./styles.module.css";
 import logo_icon from "../../assets/icons/logo_icon.svg";
 
 function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <img src={logo_icon} alt="logo_icon" />
-      <nav>
+      <nav className={styles.nav}>
         <a href="">Главная </a>
         <a href="">Музыка</a>
         <a href="">Сообщества</a>
